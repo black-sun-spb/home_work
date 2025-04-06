a7fe6d3060cb2e83d9946e02994154fc45ce8733
Taisiya Gorodnicheva <taisiya-1992@mail.ru>
2025-04-06 12:43:43 +0300

    gitignore fixes

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Маскирует номер банковской карты в формате 0000 00** **** 0000"""
+
+    str_number = str(card_number)
+    str_card_number = str_number[0:4] + " " + str_number[4:6] + "** **** " + str_number[-4:]
+    return str_card_number
+
+
+def get_mask_account(account: int) -> str:
+    """Маскирует номер банковского счета в формате **0000"""
+
+    str_account = str(account)
+    return "**" + str_account[-4:]
